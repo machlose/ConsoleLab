1ef69ebd89e5700a8d50a436b0cdc3ba43469060
Errr0 <119783076+Errr0@users.noreply.github.com>
2026-03-14 21:28:09 +0100

    Update ConsoleLab.h

--- src/ConsoleLab.h
+++ src/ConsoleLab.h
@@ -1,17 +1,28 @@
-#ifndef CONSOLELAB
-#define 
+#ifndef CONSOLELAB_H
+#define CONSOLELAB_H
 
-
-
-typedef enum {
+typedef enum clState clState;
+typedef enum clState {
     working
 } clState;
 
-typedef struct {
-    clState state;    
-} ConsoleLabContext;
+typedef enum clResult clResult;
+typedef enum clResult {
+    success,
+    failure
+} clResult;
 
-ConsoleLabContext ConsoleLabCreateContext;
+typedef struct clContext clContext;
+typedef struct clContext{
+    clState state;    
+} clContext;
+
+clContext* cl_global_context;
+
+void cl_context_create(clContext* context, clResult* result);
+#define cl_context_create(...) cl_context_create(cl_global_context, &cl_global_context->result, __VA_ARGS__)
+#define cl_context_create(result, ...) cl_context_create(cl_global_context, result, __VA_ARGS__)
+void cl_context_destroy(clContext* context, clResult* result);
 
 #else 
 #define CONSOLELAB
