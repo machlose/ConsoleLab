fec875a756f1b730d700e8472b82cd5484b66906
Errr0 <119783076+Errr0@users.noreply.github.com>
2026-04-10 21:23:00 +0200

    rewert

--- src/main.c
+++ src/main.c
@@ -1,7 +1,21 @@
 #include "ConsoleLab.h"
-#include "Views/ChessBoard.c"
 #include <stdio.h>
+
 int main(){
-    printf("%s",RenderChessBoard());
+    clConsoleSprite sprite;
+    
+    clConsoleSpriteCreate(&sprite, 4, 4, 4, 5);
     return 0;
 }
+
+
+
+
+
+// #include "ConsoleLab.h"
+// #include "Views/ChessBoard.c"
+// #include <stdio.h>
+// int main(){
+//     printf("%s",RenderChessBoard());
+//     return 0;
+// }
