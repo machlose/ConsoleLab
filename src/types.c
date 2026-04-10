2b272f5db1efdbacd8c311dbdd2b300e21fca6e9
Errr0 <119783076+Errr0@users.noreply.github.com>
2026-04-10 12:48:06 +0200

    Delete src directory

--- src/types.c
+++
@@ -1,8 +0,0 @@
-#ifndef CONSOLELAB_TYPES
-#define CONSOLECONSOLELAB_TYPESLAB_H
-typedef double cltime;
-typedef enum clState clState;
-typedef enum clResult clResult;
-typedef enum clOS clOS;
-typedef struct clContext clContext;
-#endif
