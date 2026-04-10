9d6dc79cde7dcc2ce12ed2d9cab7867c39d27b20
Errr0 <119783076+Errr0@users.noreply.github.com>
2026-04-10 12:42:54 +0200

    Add files via upload

--- src/ConsoleLab.h
+++ src/ConsoleLab.h
@@ -2,38 +2,53 @@
 #define CONSOLELAB_H
 #define CL_VERSION_MAJOR 0
 #define CL_VERSION_MINOR 0
-#include "ConsoleLabFlags.c" // delete later
-#include "types.c"
-#ifdef _WIN32
-#include <windows.h>
-#else
-//unix includes
-#endif
+
+#include <vec.c>
+// #include <stdlib.h>
+// #include <stdbool.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
-clContext* cl_global_context;
+typedef enum {
+    working
+} clState;
+
+typedef enum {
+    success,
+    context_malloc_failure,
+    failure
+} clResult;
+
+typedef enum {
+    windows,
+    unix
+} clOS;
+
+typedef enum {
+    console_mode,
+} clMode;
+
+typedef struct clWindow clWindow;
+typedef struct clContext clContext;
+
+
+
+
+clContext* clGlobalContext;
 
 // example for every function
-// void cl_context_create(clContext* context, clResult* result);
-// #define cl_(...) cl_(cl_global_context, &cl_global_context->result, __VA_ARGS__)
-// #define cl_(result, ...) cl_(cl_global_context, result, __VA_ARGS__)
+// void cl(clContext* context, clResult* result);
+// #define cl(...) cl(clGlobalContext, &clGlobalContext->result, __VA_ARGS__)
+// #define cl(result, ...) cl(clGlobalContext, result, __VA_ARGS__)
 
-void cl_context_create(clContext* context, clResult* result);
-#define cl_context_create() cl_context_create(cl_global_context, &cl_global_context->result)
-#define cl_context_create(result) cl_context_create(cl_global_context, result)
-void cl_context_destroy(clContext* context);
-#define cl_context_create() cl_context_create(cl_global_context)
-
+void clInit(clContext* context, clResult* result);
+void clClose(clContext* context, clResult* result);
 
 #ifdef __cplusplus
 }
 #endif
-#ifdef CL_AUTO_INCLUDE
-#ifdef _WIN32
-#include "ConsoleLabWin.c"
-#else
-#include "ConsoleLabUNIX.c"
-#endif
+#ifdef CL_INCLUDE_IMPLEMENTATION
+#include "ConsoleLab.c"
 #endif
 #endif
---
+++ src/ConsoleLabOld.h
@@ -0,0 +1,54 @@
+#ifndef CONSOLELAB_H
+#define CONSOLELAB_H
+#define CL_VERSION_MAJOR_H 0
+#define CL_VERSION_MINOR_H 0
+// #include "types.c"
+#ifdef _WIN32
+#include <windows.h>
+#else
+//unix includes
+#endif
+#ifdef __cplusplus
+extern "C" {
+#endif
+typedef double cltime;
+typedef enum clOS clOS;
+typedef enum clMode clMode;
+typedef enum clState clState;
+typedef enum clResult clResult;
+typedef struct clWindow clWindow;
+typedef struct clContext clContext;
+clContext* cl_global_context;
+
+
+
+//https://www.lookuptables.com/text/extended-ascii-table
+
+// example for every function
+// void cl_(clContext* context, clResult* result);
+// #define cl_(...) cl_(cl_global_context, &cl_global_context->result, __VA_ARGS__)
+// #define cl_(result, ...) cl_(cl_global_context, result, __VA_ARGS__)
+
+void cl_create_context(clContext* context, clResult* result);
+#define cl_create_context() cl_create_context(cl_global_context, &cl_global_context->result)
+#define cl_create_context(result) cl_create_context(cl_global_context, result)
+void cl_destroy_context(clContext* context);
+#define cl_destroy_context() cl_destroy_context(cl_global_context)
+void cl_create_window(clContext* context, clResult* result);
+void cl_destroy_window(clContext* context);
+void cl_get_console_handle(clContext* context, clResult* result);
+void cl_set_console_buffer(clContext* context, clResult* result);
+void cl_draw_sprite(clContext* context, clResult* result);
+
+
+#ifdef __cplusplus
+}
+#endif
+#ifdef CL_AUTO_INCLUDE
+#ifdef _WIN32
+#include "ConsoleLabWin.c"
+#else
+#include "ConsoleLabUNIX.c"
+#endif
+#endif
+#endif
