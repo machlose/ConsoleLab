fbf52cf9926d66315ecca272f059ffe9783ca999
Errr0 <119783076+Errr0@users.noreply.github.com>
2026-04-15 21:04:47 +0200

    set cursor pos

--- src/ConsoleLab.c
+++ src/ConsoleLab.c
@@ -12,7 +12,14 @@
 #include "ConsoleLab_unix.c"
 #endif
 
-
+void setCursorPos(int x, int y) {
+#ifdef _WIN32
+    COORD coord = {x, y};
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+#else
+    printf("\033[%d;%dH", y + 1, x + 1);
+#endif
+}
 
 // void clInit(clContext* context, clResult* result){
     
