7a5be613b7efee5af321930e05341eeb31edc44c
Errr0 <119783076+Errr0@users.noreply.github.com>
2026-04-21 19:30:22 +0200

    szszownica

--- src/test.c
+++ src/test.c
@@ -10,98 +10,113 @@
 #define COL_CYAN   6
 #define COL_MAGENTA 7
 #define COL_GRAY   8
- 
+#define COL_TRANSPARENT   255
+
 int main() {
     ConsoleLab_Init();
  
     int playerX = 10, playerY = 5;
     int frame = 0;
- 
+    int cellSize = 6;
+    bool swapColors = false;
+    vec3i cells[8][8];
+    for (size_t i = 0; i < 8; i++)
+    {
+        for (size_t j = 0; j < 8; j++)
+        {
+            cells[j][i] = (vec3i){2+cellSize*2*j, 1+cellSize*i, 0};
+        }
+    }
+    
+    Sprite cell;
+    cell.position = (vec3i){0, 0, 0};
+    cell.dimensions.width  = 2;
+    cell.dimensions.height = 1;
+    Sprite_AllocateBuffer(&cell);
+
+    Sprite field;
+    field.position = (vec3i){0, 0, 0};
+    field.dimensions.width  = cellSize*2;
+    field.dimensions.height = cellSize;
+    Sprite_AllocateBuffer(&field);
+
+    // Sprite pawn;
+    // pawn.position = (vec3i){10, 5, 0};
+    // pawn.dimensions.width  = 12;
+    // pawn.dimensions.height = 6;
+    // Sprite_AllocateBuffer(&pawn);
+
+    // Sprite_DrawWStringWrapped(&pawn, 0, 0, U"                 ▄▄         ████         ██         ▄██▄       ██████   ", COL_WHITE, 255);
+
     while (1) {
         ConsoleLab_Update();
  
         Input* input = &ConsoleLab_GlobalContext.input;
  
-        // Wyjscie
         if (InputKeyPressed(input, KEY_ESCAPE)) break;
  
-        // Ruch gracza
         if (InputKeyPressed(input, KEY_W) || InputKeyPressed(input, KEY_UP))    playerY--;
         if (InputKeyPressed(input, KEY_S) || InputKeyPressed(input, KEY_DOWN))   playerY++;
         if (InputKeyPressed(input, KEY_A) || InputKeyPressed(input, KEY_LEFT))   playerX--;
         if (InputKeyPressed(input, KEY_D) || InputKeyPressed(input, KEY_RIGHT))  playerX++;
  
-        // Rysuj
-        ClearBuffer(COL_BLACK);
- 
-        // Ramka
-        vec2i size = ConsoleLab_GlobalContext.buffer.current->dimensions;
-        for (int x = 0; x < size.width; x++) {
-            DrawChar(x, 0,          U'─', COL_CYAN, COL_BLACK);
-            DrawChar(x, size.height-1, U'─', COL_CYAN, COL_BLACK);
-        }
-        for (int y = 1; y < size.height-1; y++) {
-            DrawChar(0,           y, U'│', COL_CYAN, COL_BLACK);
-            DrawChar(size.width-1, y, U'│', COL_CYAN, COL_BLACK);
-        }
-        DrawChar(0,            0,           U'┌', COL_CYAN, COL_BLACK);
-        DrawChar(size.width-1, 0,           U'┐', COL_CYAN, COL_BLACK);
-        DrawChar(0,            size.height-1, U'└', COL_CYAN, COL_BLACK);
-        DrawChar(size.width-1, size.height-1, U'┘', COL_CYAN, COL_BLACK);
- 
-        // Gracz
-        DrawChar(playerX, playerY, U'@', COL_YELLOW, COL_BLACK);
- 
-        // UI - pozycja myszy
-        char info[64];
-        sprintf(info, "Mysz: %d,%d  Kolko: %d  Klatka: %d",
-            input->mousePosition.x, input->mousePosition.y,
-            input->mouseWheel, frame);
-        CL_DrawText(2, 1, info, COL_WHITE, COL_BLACK);
- 
-        CL_DrawText(2, 2, "WASD/strzalki = ruch  ESC = wyjscie", COL_GRAY, COL_BLACK);
- 
-        // Kursor myszy
-        DrawChar(input->mousePosition.x, input->mousePosition.y,
-                 U'▪', COL_RED, COL_BLACK);
- 
-        // Animacja w rogu
-        const char* anim[] = {"|", "/", "-", "\\"};
-        CL_DrawText(size.width - 3, 1, anim[frame % 4], COL_GREEN, COL_BLACK);
- 
-        // LPM = rysuj znak w miejscu kursora
-        if (InputKeyDown(input, MOUSE_LEFT)) {
-            DrawChar(input->mousePosition.x, input->mousePosition.y,
-                     U'█', COL_MAGENTA, COL_BLACK);
-        }
-        
-        Sprite pawn;
-        pawn.position = (vec3i){10, 5, 0};  // albo ręcznie .x=10 .y=5 .z=0
-        pawn.dimensions.width  = 12;
-        pawn.dimensions.height = 6;
-        Sprite_AllocateBuffer(&pawn);
+        ClearBuffer(COL_MAGENTA);
 
-        Sprite_DrawWStringWrapped(&pawn, 0, 0, U"                 ▄▄         ████         ██         ▄██▄       ██████   ", COL_WHITE, 255);
-        
-        Sprite bg;
-        bg.position = (vec3i){10, 5, 0};  // albo ręcznie .x=10 .y=5 .z=0
-        bg.dimensions.width  = 12;
-        bg.dimensions.height = 6;
-        Sprite_AllocateBuffer(&bg);
-        
-        Sprite_DrawWStringWrapped(&bg, 0, 0, U"                                                                        ", COL_WHITE, COL_MAGENTA);
+        swapColors = true;
+        Sprite_DrawWStringWrapped(&cell, 0, 0, U"██", swapColors ? COL_BLACK : COL_WHITE, 255);
 
-        DrawToBufferOpaque(&bg);
-        bg.position = (vec3i){22, 5, 0};
-        Sprite_DrawWStringWrapped(&bg, 0, 0, U"                                                                        ", COL_BLACK, COL_WHITE);
-        DrawToBufferOpaque(&bg);
-        DrawToBuffer(&pawn);
-        
+        cell.position = (vec3i){0, 0, 0};
+        DrawToBuffer(&cell);
+
+        cell.position = (vec3i){cellSize*2*8+2, cellSize*8+1, 0};
+        DrawToBuffer(&cell);
+
+        swapColors = !swapColors;
+        Sprite_DrawWStringWrapped(&cell, 0, 0, U"██", swapColors ? COL_BLACK : COL_WHITE, 255);
+
+        cell.position = (vec3i){0, cellSize*8+1, 0};
+        DrawToBuffer(&cell);
+
+        cell.position = (vec3i){cellSize*2*8+2, 0, 0};
+        DrawToBuffer(&cell);
+
+        for (size_t i = 0; i < 8; i++)
+        {
+            for (size_t j = 0; j < cellSize; j++)
+            {
+                cell.position = (vec3i){j*2+2+i*cellSize*2, 0, 0};
+                DrawToBuffer(&cell);
+                cell.position = (vec3i){0, j+1+i*cellSize, 0};
+                DrawToBuffer(&cell);
+            }
+            swapColors = !swapColors;
+            Sprite_DrawWStringWrapped(&cell, 0, 0, U"██", swapColors ? COL_BLACK : COL_WHITE, 255);
+            for (size_t j = 0; j < cellSize; j++)
+            {
+                cell.position = (vec3i){j*2+2+i*cellSize*2, cellSize*8+1, 0};
+                DrawToBuffer(&cell);
+                cell.position = (vec3i){cellSize*2*8+2, j+1+i*cellSize, 0};
+                DrawToBuffer(&cell);
+            }
+        }
+
+        for (size_t i = 0; i < 8; i++)
+        {
+            for (size_t j = 0; j < 8; j++)
+            {
+                swapColors = !swapColors;
+                Sprite_DrawWStringWrapped(&field, 0, 0, U"                                                                        ", COL_TRANSPARENT, swapColors ? COL_BLACK : COL_WHITE);
+                field.position = cells[i][j];
+                DrawToBufferOpaque(&field);
+            }
+            swapColors = !swapColors;
+        }
+       
 
         ConsoleLabFlipBuffer();
         frame++;
  
-        Sleep(16); // ~60fps
+        Sleep(16);
     }
  
     return 0;
